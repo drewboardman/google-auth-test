ac477dbe461ca0ba9dea537a4d97d101bd8d6908
Drew Boardman <drew.e.boardman@gmail.com>
2022-12-24 12:35:38 -0500

    everything working with signing in a redirecting

--- src/pages/AuthContext.ts
+++ src/pages/AuthContext.ts
@@ -1,10 +1,8 @@
 import React from "react";
 
-interface AuthContextType {
+export interface AuthContextType {
   isAuthenticated: boolean;
   login: () => void;
 }
 
-const AuthContext = React.createContext<undefined | AuthContextType>(undefined);
-
-export default AuthContext;
+export const AuthContext = React.createContext<undefined | AuthContextType>(undefined);
